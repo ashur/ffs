141d23725ee1bea38943ef85d103435f5735d0a8
Ashur Cabrera <hello@ashur.cab>
2019-07-14 16:00:55 +0000

    Add Shell.parse, Shell.exec

--- src/shell.js
+++ src/shell.js
@@ -21,15 +21,63 @@ class Shell
 		let currentDirectory = this.filesystem.getNodeFromPath( absolutePathname );
 		if( currentDirectory instanceof File )
 		{
-			throw new Error( 'Not a directory' );
+			throw new Error( `not a directory: ${pathname}` );
 		}
 
 		this.currentDirectory = currentDirectory;
 	}
 
+	execute( { command, args=[] } )
+	{
+		let output = [];
+
+		switch( command )
+		{
+			case 'cd':
+				try
+				{
+					this.cd( args[0] );
+				}
+				catch( error )
+				{
+					output.push( `cd: ${error.message}` );
+				}
+
+				break;
+
+			case 'echo':
+				output.push( args.join( ' ' ) );
+				break;
+
+			case 'pwd':
+				output.push( this.currentDirectory.path );
+				break;
+
+			default:
+				output.push( `command not found: ${command}` );
+				break;
+		}
+
+		return output;
+	}
+
 	get cwd()
 	{
 		return this.currentDirectory;
+	}
+
+	static parse( input )
+	{
+		let components = input
+			.trim()
+			.split( ' ' );
+
+		let hash = {
+			command: components.shift(),
+			args: components,
+		};
+
+		return hash;
 	}
 }
 
--- test/shell.js
+++ test/shell.js
@@ -27,6 +27,11 @@ describe( 'Shell', function()
 			{
 				shell.cd( '/invalid/path' );
 			});
+
+			assert.throws( () =>
+			{
+				shell.cd( 'invalid' );
+			});
 		});
 
 		it( 'should throw if attempting to change to file', function()
@@ -66,7 +71,7 @@ describe( 'Shell', function()
 			filesystem.root
 				.add( new Directory( 'home' ) )
 				.add( new Directory( 'ashur' ) )
-		
+
 			shell.cd( 'home' );
 			assert.equal( shell.cwd.path, '/home' );
 
@@ -81,6 +86,95 @@ describe( 'Shell', function()
 
 			shell.cd( '../' );
 			assert.equal( shell.cwd.path, '/' );
+		});
+	});
+
+	describe( '#parse', function()
+	{
+		it( 'should return hash', function()
+		{
+			let hash = Shell.parse( 'cd home/ashur ' );
+			assert.deepEqual( hash, {
+				command: 'cd',
+				args: [
+				    'home/ashur'
+				]
+			});
+		});
+	});
+
+	describe( '#execute', function()
+	{
+		it( 'should output an error if command is not found', function()
+		{
+			let shell = new Shell( new FileSystem() );
+
+			let hash = { command: 'foo' };
+			let expectedOutput = [`command not found: ${hash.command}`];
+
+			assert.deepEqual( shell.execute( hash ), expectedOutput );
+		});
+
+		context( 'cd', function()
+		{
+			it( 'should change working directory', function()
+			{
+				let filesystem = new FileSystem();
+
+				filesystem.root
+					.add( new Directory( 'home' ) )
+					.add( new Directory( 'ashur' ) )
+
+				let hash = {
+					command: 'cd',
+					args: [ 'home/ashur' ]
+				};
+
+				let shell = new Shell( filesystem );
+				assert.deepEqual( shell.execute( hash ), [] );
+
+				assert.equal( shell.cwd.path, '/home/ashur' );
+			});
+		})
+
+		context( 'echo', function()
+		{
+			it( 'should output a line of text', function()
+			{
+				let hash = {
+					command: 'echo',
+					args: [ 'hello', 'world', ]
+				};
+
+				let shell = new Shell( new FileSystem() );
+				let expectedOutput = ['hello world'];
+
+				assert.deepEqual( shell.execute( hash ), expectedOutput );
+			});
+		});
+
+		context( 'pwd', function()
+		{
+			it( 'should output working directory path', function()
+			{
+				let hash = { command: 'pwd' };
+
+				let filesystem = new FileSystem();
+
+				filesystem.root
+					.add( new Directory( 'home' ) )
+					.add( new Directory( 'ashur' ) )
+
+				let shell = new Shell( filesystem );
+
+				assert.deepEqual( shell.execute( hash ), ['/'] );
+
+				shell.cd( 'home' );
+				assert.deepEqual( shell.execute( hash ), ['/home'] );
+
+				shell.cd( 'ashur' );
+				assert.deepEqual( shell.execute( hash ), ['/home/ashur'] );
+			});
 		});
 	});
 });
